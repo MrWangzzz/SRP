7d4b4e06b18bf9547794ff4306f064545498c9a6
Wzy <84059192 + MrWangzzz@users.noreply.github.com>
2021-09-10 21:11:43 +0800

    Add：直接渲染在屏幕上，渲染cube跟skyBox

---
+++ Assets/RenderDraw.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderDraw : MonoBehaviour
+{
+
+    private RenderTexture renderTexture;
+    public Material prueColormaterial;
+    public Material skyboxmaterial;
+    public Mesh CubeMesh;
+    public Transform[] Cube;
+    private Mesh m_mesh;
+    private Vector4[] corners = new Vector4[4];
+    private Mesh fullScreenMesh 
+    {
+        get
+        {
+            if (m_mesh != null)
+            {
+                return m_mesh;
+            }
+            m_mesh = new Mesh();
+            m_mesh.vertices = new Vector3[]
+            {
+                new Vector3(-1,-1,0),
+                new Vector3(-1,1,0),
+                new Vector3(1,1,0),
+                new Vector3(1,-1,0)
+            };
+            m_mesh.uv = new Vector2[]
+            {
+                new Vector2(0,1),
+                new Vector2(0,0),
+                new Vector2(1,0),
+                new Vector2(1,1)
+
+            };
+            m_mesh.SetIndices(new int[] { 0, 1, 2, 3 }, MeshTopology.Quads,0);
+            return m_mesh;
+        }
+    }
+    void Start()
+    {
+        renderTexture = new RenderTexture(Screen.width, Screen.height,24);
+    }
+
+    // Update is called once per frame
+
+    private void OnPostRender()
+    {
+        Camera camera = Camera.current;
+        Graphics.SetRenderTarget(renderTexture);
+        GL.Clear(true, true, Color.gray);
+        prueColormaterial.color = new Color(0, 0.5f, 1);
+        prueColormaterial.SetPass(0);
+        foreach (var i in Cube) 
+        { 
+        Graphics.DrawMeshNow(CubeMesh, i.localToWorldMatrix);
+        }
+        DrawSkyBox(camera);
+
+        Graphics.Blit(renderTexture, camera.targetTexture);
+    }
+    /// <summary>
+    /// 绘制天空盒
+    /// </summary>
+    /// <param name="camera"></param>
+    private void DrawSkyBox(Camera camera) 
+    {
+        corners[0] = camera.ViewportToWorldPoint(new Vector3(0, 0, camera.farClipPlane));
+        corners[1] = camera.ViewportToWorldPoint(new Vector3(1, 0, camera.farClipPlane));
+        corners[2] = camera.ViewportToWorldPoint(new Vector3(0, 1, camera.farClipPlane));
+        corners[3] = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.farClipPlane));
+        skyboxmaterial.SetVectorArray("_corners", corners);
+        skyboxmaterial.SetPass(0);
+        Graphics.DrawMeshNow(fullScreenMesh, Matrix4x4.identity);
+    }
+}
---
+++ Assets/SkyBox.shader
@@ -0,0 +1,54 @@
+﻿Shader "Unlit/SkyBox"
+{
+    Properties
+    {
+     _SkyBoxTexture("SkyBox Texture",Cube) = "White"{}
+    }
+    SubShader
+    {
+        Cull Off
+        ZWrite Off
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+                float3 worldPos:TEXCOORD1;
+            };
+
+            float4 _corners[4];
+            TextureCube _SkyBoxTexture;
+            SamplerState sampler_SkyBoxTexture;
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = v.vertex;
+                o.worldPos = _corners[v.uv.x + v.uv.y*2].xyz;
+                return o;
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                fixed3 viewDir = normalize(i.worldPos - _WorldSpaceCameraPos);
+                return _SkyBoxTexture.Sample(sampler_SkyBoxTexture, viewDir);
+            }
+            ENDCG
+        }
+    }
+}
